562e0e3c9dcc9500776c8c1744af12e5e2367274
PFArribas <palomafa82@educastur.es>
2022-04-21 18:34:46 +0200

    Version sin comentarios y sin metodos

---
+++ src/Calculadora/CalcuSencilla.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+public class CalcuSencilla {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		// Programa para tag 1 - sin comntarios y sin metodos
+		System.out.println("Programa para version 0 - sin comntarios y sin metodos");
+		System.out.println("Comentarios: ------------------");
+		System.out.println("Metodos: ------------------");
+
+	}
+
+}
